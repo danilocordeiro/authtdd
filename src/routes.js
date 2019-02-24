e986c8c13aab4fab41d6bb21ee0c7cc1cf668744
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2019-02-24 13:03:48 -0300

    Add jest

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,3 @@
 const routes = require('express').Router();
-const { User } = require('./app/models');
-
-User.create({
-  name: "Cordeiro", 
-  email: "cord@gmail.com",
-  password_hash: "iouijklghj123"
-})
 
 module.exports = routes;
