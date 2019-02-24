84b263f2757a52106b6c945fa75db926b664bb52
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2019-02-24 18:58:31 -0300

    Add coverage

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -66,6 +66,24 @@ describe('Authentication', () => {
     expect(response.status).toBe(200);
   });
 
+  it('should not be able to access private routes without jwt token', async() => {
+    
+    const response = await request(app)
+      .get('/dashboard');
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to access private routes with invalid jwt token', async() => {
+    
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `123456`);
+
+    expect(response.status).toBe(401);
+  });
+
+
 
 });
 
