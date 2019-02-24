fdf7f1f72cd6a516a50213f7b5619e66a56242aa
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2019-02-24 17:50:20 -0300

    Add factory and faker

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 
 const app = require('../../src/app');
-const { User } = require('../../src/app/models');
 const truncate = require('../utils/truncate');
+const factory = require('../factories');
 
 describe('Authentication', () => {
   beforeEach(async() => {
@@ -10,27 +10,23 @@ describe('Authentication', () => {
   })
 
   it('should authenticated with valid credentials', async() => {
-    const user = await User.create({ 
-      name: 'Juliana', 
-      email: 'ju@gata.com', 
-      password: 'hjk123'
+    const user = await factory.create('User', {
+      password: '123456'
     });
 
     const response = await request(app)
       .post('/sessions')
       .send({
         email: user.email,
-        password: 'hjk123'
+        password: '123456'
       });
 
     expect(response.status).toBe(200);
   });
 
   it('should not authenticated with invalid credentials', async() => {
-    const user = await User.create({ 
-      name: 'Juliana', 
-      email: 'ju@gata.com', 
-      password: '123433'
+    const user = await factory.create('User', {
+      password: '123123'
     });
 
     const response = await request(app)
@@ -44,20 +40,32 @@ describe('Authentication', () => {
   });
 
   it('should return jwt token when authenticated', async() => {
-    const user = await User.create({ 
-      name: 'Juliana', 
-      email: 'ju@gata.com', 
-      password: 'hjk123'
+    const user = await factory.create('User', {
+      password: '123456'
     });
 
     const response = await request(app)
       .post('/sessions')
       .send({
         email: user.email,
-        password: 'hjk123'
+        password: '123456'
       });
 
     expect(response.body).toHaveProperty('token');
   });
+
+  it('should be able to access private routes when authenticated', async() => {
+    const user = await factory.create('User', {
+      password: '123456'
+    });
+
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer ${user.generateToken()}`);
+
+    expect(response.status).toBe(200);
+  });
+
+
 });
 
